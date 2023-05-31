7f4efcf830b7452ac9c91018adc934277dc49667
Vildarodinson <117757981+Vildarodinson@users.noreply.github.com>
2023-05-31 08:15:40 -0800

    fix timestamp

--- cctv/cctv.py
+++ cctv/cctv.py
@@ -11,7 +11,6 @@ def choose_folder():
     return folder_path
 
 def save_video():
-    # Prompt user to choose folder
     choice = input("Choose an option:\n1. Use an existing folder\n2. Create a new folder\n")
     if choice == '1':
         folder = choose_folder()
@@ -19,36 +18,51 @@ def save_video():
         folder = os.path.join(os.path.expanduser("~"), "Videos", "recordings")
         os.makedirs(folder, exist_ok=True)
 
-    # Get the current date and time
-    now = datetime.now()
-    timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+    video_capture = cv2.VideoCapture(0)
 
-    # Set up video capture
-    video_capture = cv2.VideoCapture(0)  # Change the index to use a different webcam
-
-    # Set up video writer
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    filename = os.path.join(folder, f'{timestamp}.avi')
-    video_writer = cv2.VideoWriter(filename, fourcc, 20.0, (640, 480))
 
-    # Start recording
     while True:
-        # Capture frame-by-frame
-        ret, frame = video_capture.read()
-        # Write the frame to the video file
-        video_writer.write(frame)
-        # Display the frame
-        cv2.imshow('CCTV', frame)
-        cv2.namedWindow('CCTV', cv2.WINDOW_NORMAL)
-        # Stop recording when 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    # Release the video capture and writer
-    video_capture.release()
-    video_writer.release()
+        video_writer = None
 
-    # Close all windows
+        while video_capture.isOpened():
+            ret, frame = video_capture.read()
+
+            now = datetime.now()
+            date_str = now.strftime("%Y-%m-%d")
+            time_str = now.strftime("%H:%M:%S")
+            timestamp = f"Date: {date_str} Time: {time_str}"
+
+            if video_writer is None:
+                filename = os.path.join(folder, f'{timestamp}.avi')
+                video_writer = cv2.VideoWriter(filename, fourcc, 20.0, (640, 480))
+
+            text = timestamp
+            text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)[0]
+            text_pos = (frame.shape[1] - text_size[0] - 10, frame.shape[0] - 10)
+
+            cv2.putText(frame, text, text_pos, cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
+
+            video_writer.write(frame)
+
+            cv2.imshow('CCTV', frame)
+            cv2.namedWindow('CCTV', cv2.WINDOW_NORMAL)
+
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
+            elif key == ord('1'):
+                break
+
+            now = datetime.now()  # Update the current time
+
+        if video_writer is not None:
+            video_writer.release()
+
+        if key == ord('q'):
+            break
+
+    video_capture.release()
     cv2.destroyAllWindows()
 
-# Run the program
 save_video()
